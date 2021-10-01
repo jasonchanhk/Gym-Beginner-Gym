84917edddd0d31d951b1fb25d3a33d0fa14a6202
jasonchanhk <chanjky97@gmail.com>
2021-10-01 14:46:11 +0100

    ready for deploy

---
+++ src/Components/HeaderComponent.js
@@ -0,0 +1,40 @@
+import React , { Component } from "react";
+import {Navbar, Container, Nav} from "react-bootstrap";
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <Navbar expand="lg" bg="light">
+                    <Container>
+                        <Navbar.Brand href="/home" className="coloredText">
+                            <img
+                            src="/barbell.svg"
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top me-2"
+                            alt="Gym Beginner Guide Logo"
+                            />
+                            Gym Beginner Guide
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="/home">Home</Nav.Link>
+                                <Nav.Link href="/workout">Workout</Nav.Link> 
+                                <Nav.Link href="/nutrition">Nutrition</Nav.Link>                                                              
+                                <Nav.Link href="/gymnearby">Gym</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>                
+            </div>
+        )
+    }
+}
+
+export default Header;
